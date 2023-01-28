f8e5d9c1bf8335067c665e6ca7e0af07dfd42d7f
liuyuhao <liu.yu.hao@qq.com>
2023-01-28 17:52:43 +0800

    feat: cloud dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Author: liuyuhao
+ * @Date: 2023-01-28 15:06:21
+ * @LastEditors: liuyuhao
+ * @LastEditTime: 2023-01-28 17:49:04
+ * @Description:
+ */
+import React, { Suspense } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Cloud, Sky } from '@react-three/drei'
+import logo from './logo.svg'
+import './App.css'
+
+function Rig() {
+  const camera = useThree(state => state.camera)
+  return useFrame(state => {
+    camera.position.z = Math.sin(state.clock.elapsedTime) * 20
+  })
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
+        </p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
+      <Canvas camera={{ position: [0, 0, 16], fov: 75 }}>
+        <ambientLight intensity={0.8} />
+        <pointLight intensity={2} position={[0, 0, -1000]} />
+        <Suspense fallback={null}>
+          <Cloud position={[-4, -2, -25]} speed={0.2} opacity={1} />
+          <Cloud position={[4, 2, -15]} speed={0.2} opacity={0.5} />
+          <Cloud position={[-4, 2, -10]} speed={0.2} opacity={1} />
+          <Cloud position={[4, -2, -5]} speed={0.2} opacity={0.5} />
+          <Cloud position={[4, 2, 0]} speed={0.2} opacity={0.75} />
+        </Suspense>
+        <Sky azimuth={0.1} turbidity={10} rayleigh={0.5} inclination={0.6} distance={1000} />
+        <Rig />
+      </Canvas>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Author: liuyuhao
+ * @Date: 2023-01-28 15:06:21
+ * @LastEditors: liuyuhao
+ * @LastEditTime: 2023-01-28 17:25:42
+ * @Description:
+ */
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
