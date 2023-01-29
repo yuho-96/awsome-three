c58c55a9ae82464e755c2a176ffdbe6921e92642
liuyuhao <liu.yu.hao@qq.com>
2023-01-29 14:35:28 +0800

    feat: home develop

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,33 +2,32 @@
  * @Author: liuyuhao
  * @Date: 2023-01-29 10:40:10
  * @LastEditors: liuyuhao
- * @LastEditTime: 2023-01-29 11:48:54
+ * @LastEditTime: 2023-01-29 14:33:29
  * @Description:
  */
-import styles from './home.module.less'
+import styles from './index.module.less'
 
 function Home() {
   return (
-    <div>
-      <div
+    <div
+      className={styles.content}
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate3d(-50%,-50%,0)',
+      }}>
+        <div className={styles.text}>test</div>
+      <h1
         style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate3d(-50%,-50%,0)',
+          margin: 0,
+          padding: 0,
+          fontSize: '15em',
+          fontWeight: 500,
+          letterSpacing: '-0.05em',
         }}>
-        <h1
-          className={styles.text}
-          style={{
-            margin: 0,
-            padding: 0,
-            fontSize: '15em',
-            fontWeight: 500,
-            letterSpacing: '-0.05em',
-          }}>
-          hello
-        </h1>
-      </div>
+        hello
+      </h1>
     </div>
   )
 }
