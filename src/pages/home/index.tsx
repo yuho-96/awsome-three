a1bfdbf5c36fb251d66cc497ada389763b58e632
liuyuhao <liu.yu.hao@qq.com>
2023-01-29 15:01:44 +0800

    update home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,29 +2,27 @@
  * @Author: liuyuhao
  * @Date: 2023-01-29 10:40:10
  * @LastEditors: liuyuhao
- * @LastEditTime: 2023-01-29 14:33:29
+ * @LastEditTime: 2023-01-29 15:00:22
  * @Description:
  */
-import styles from './index.module.less'
+import styles from './index.module.css'
 
 function Home() {
   return (
     <div
-      className={styles.content}
       style={{
         position: 'absolute',
         top: '50%',
         left: '50%',
         transform: 'translate3d(-50%,-50%,0)',
       }}>
-        <div className={styles.text}>test</div>
       <h1
+        className={styles.text}
         style={{
           margin: 0,
           padding: 0,
           fontSize: '15em',
           fontWeight: 500,
-          letterSpacing: '-0.05em',
         }}>
         hello
       </h1>
